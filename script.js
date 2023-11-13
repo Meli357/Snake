156b948831f521c8eeab5ce678e4ae230e1a3708
Melissa Flores <106169198+Meli357@users.noreply.github.com>
2023-11-13 16:03:38 +0100

    add: variables for coordinates, inside initGame statement to change movement direction of snake, new function to use coordinates of snake and condition to make appear new fruit

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let playBoard = document.querySelector(".play-board");
 let foodX, foodY;
 let snakeX = 5,
   snakeY = 10;
+let velocityX = 0,
+  velocityY = 0;
 
 //function to make food appear randomly
 const changeFoodPosition = () => {
@@ -16,8 +18,40 @@ changeFoodPosition();
 //function to display within the play board our snake and fruit
 const initGame = () => {
   let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+  //if condition is true change position of new fruit
+  if (snakeX === foodX && snakeY === foodY) {
+    changeFoodPosition();
+  }
+
+  //position of snake changes according to key pressed
+  snakeX += velocityX;
+  snakeY += velocityY;
+
   htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
 };
 
-initGame();
+//to change direction of snake based on coordinates
+const changeDirection = (e) => {
+  if (e.key === "ArrowUp") {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.key === "ArrowDown") {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.key === "ArrowLeft") {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.key === "ArrowRight") {
+    velocityX = 1;
+    velocityY = 0;
+  }
+  //initGame();
+};
+
+//initGame();
+
+setInterval(initGame, 125);
+
+document.addEventListener("keydown", changeDirection);
