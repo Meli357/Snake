14d42f13bb22121c0aa42b9e0a59f70305ddafbf
Melissa Flores <106169198+Meli357@users.noreply.github.com>
2023-11-15 18:35:23 +0100

    set limit on the field with handleGameOver function and using snake coordinates

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let playBoard = document.querySelector(".play-board");
 
 //create variables that will change its value
+
+let gameOver = false;
+
 let foodX, foodY;
 let snakeX = 5,
   snakeY = 10;
 let velocityX = 0,
   velocityY = 0;
+
+let setIntervalId;
 
 //new array
 let snakeBody = [];
@@ -20,6 +25,9 @@ changeFoodPosition();
 
 //function to display within the play board our snake and fruit
 const initGame = () => {
+  //checks if gameover is true
+  if (gameOver) return handleGameOver();
+
   let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
   //if condition is true change position of new fruit
@@ -43,6 +51,11 @@ const initGame = () => {
   snakeX += velocityX;
   snakeY += velocityY;
 
+  //condition that checks if snake is out of box
+  if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+    gameOver = true;
+  }
+
   //to create element for snake after eating fruit
   for (let index = 0; index < snakeBody.length; index++) {
     htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[index][1]} / ${snakeBody[index][0]}"></div>`;
@@ -50,18 +63,26 @@ const initGame = () => {
   playBoard.innerHTML = htmlMarkup;
 };
 
+//shows alert and refreshes page if snake touches border
+const handleGameOver = () => {
+  clearInterval(setIntervalId);
+  alert("Game Over!");
+  location.reload();
+};
+
 //to change direction of snake based on coordinates
+//add second condition with velocity
 const changeDirection = (e) => {
-  if (e.key === "ArrowUp") {
+  if (e.key === "ArrowUp" && velocityY != 1) {
     velocityX = 0;
     velocityY = -1;
-  } else if (e.key === "ArrowDown") {
+  } else if (e.key === "ArrowDown" && velocityY != -1) {
     velocityX = 0;
     velocityY = 1;
-  } else if (e.key === "ArrowLeft") {
+  } else if (e.key === "ArrowLeft" && velocityX != 1) {
     velocityX = -1;
     velocityY = 0;
-  } else if (e.key === "ArrowRight") {
+  } else if (e.key === "ArrowRight" && velocityX != -1) {
     velocityX = 1;
     velocityY = 0;
   }
@@ -70,6 +91,6 @@ const changeDirection = (e) => {
 
 //initGame();
 
-setInterval(initGame, 125);
+setIntervalId = setInterval(initGame, 125);
 
 document.addEventListener("keydown", changeDirection);
